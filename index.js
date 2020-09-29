72a8c3d9482b63b86a7fe8f9d68c4e8295f9f9fc
phv1710303 <1710303@dlu.edu.vn>
2020-09-29 14:32:26 +0700

    ❤❤❤

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var id = $("#studentId").val();
         var linkImg = "http://online.dlu.edu.vn/HinhSV/" + id + ".jpg";
         var outerHTML = '<img id="studentImage" alt="Sinh viên này méo có ảnh nhé!" src="' + linkImg + '">';
-        $("#studentImage")[0].outerHTML = outerHTML;
-        $("#link").text(linkImg);
+        $("#studentImage").attr('src', linkImg);
+        $("#link").text($("#studentImage").attr('src'));
     })
 })
